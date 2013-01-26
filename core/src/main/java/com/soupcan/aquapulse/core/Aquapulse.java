bbc9741062da2337184e95132d45ff6f194052e9
Zachary Latta <zchlatta@gmail.com>
2013-01-26 02:49:33 -0800

    Added screens mechanism.

--- core/src/main/java/com/soupcan/aquapulse/core/Aquapulse.java
+++ core/src/main/java/com/soupcan/aquapulse/core/Aquapulse.java
@@ -1,50 +1,17 @@
 package com.soupcan.aquapulse.core;
 
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Game;
+import com.soupcan.aquapulse.core.view.screen.GameScreen;
 
-public class Aquapulse implements ApplicationListener
+public class Aquapulse extends Game
 {
-    Texture texture;
-    SpriteBatch batch;
+    public GameScreen gameScreen = null;
 
     @Override
     public void create()
     {
-        texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
-        batch = new SpriteBatch();
-    }
+        gameScreen = new GameScreen();
 
-    @Override
-    public void resize(int width, int height)
-    {
-    }
-
-    @Override
-    public void render()
-    {
-        Gdx.gl.glClearColor(0, 0, 0, 0);
-        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(texture, 100, 100);
-        batch.end();
-    }
-
-    @Override
-    public void pause()
-    {
-    }
-
-    @Override
-    public void resume()
-    {
-    }
-
-    @Override
-    public void dispose()
-    {
+        setScreen(gameScreen);
     }
 }
