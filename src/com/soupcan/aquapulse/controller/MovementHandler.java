5fc193af3c76896522e090b14e3a058742bebc53
crynix <zachl92245>
2013-01-26 23:08:29 -0800

    Begininngs of movement.

---
+++ src/com/soupcan/aquapulse/controller/MovementHandler.java
@@ -0,0 +1,12 @@
+package com.soupcan.aquapulse.controller;
+
+import org.newdawn.slick.Input;
+
+public class MovementHandler
+{
+    public static MovementHandler Instance = new MovementHandler();
+
+    public void processInput(Input input)
+    {
+    }
+}
