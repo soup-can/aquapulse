762cbe1af624c50dcdc66dd97e447adc98981ac6
Stan Volchenok <stan.volchenok@gmail.com>
2013-01-27 12:59:57 -0800

    Added beginnings of main menu.

---
+++ src/com/soupcan/aquapulse/state/MainMenuState.java
@@ -0,0 +1,42 @@
+package com.soupcan.aquapulse.state;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenuState extends BasicGameState
+{
+    private int stateId;
+
+    public MainMenuState(int stateId)
+    {
+        this.stateId = stateId;
+    }
+
+    @Override
+    public int getID()
+    {
+        return stateId;
+    }
+
+    @Override
+    public void init(GameContainer container, StateBasedGame stateBasedGame) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException
+    {
+
+    }
+
+    @Override
+    public void update(GameContainer container, StateBasedGame stateBasedGame, int i) throws SlickException
+    {
+
+    }
+}
