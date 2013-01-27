86c5416d4513d789220517c1473e643dfe01fdde
crynix <zachl92245>
2013-01-26 21:54:24 -0800

    Added background image.

--- src/com/soupcan/aquapulse/state/GameplayState.java
+++ src/com/soupcan/aquapulse/state/GameplayState.java
@@ -2,6 +2,7 @@ package com.soupcan.aquapulse.state;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -9,6 +10,8 @@ import org.newdawn.slick.state.StateBasedGame;
 public class GameplayState extends BasicGameState
 {
     private int stateID;
+
+    private Image background;
 
     public GameplayState(int stateID)
     {
@@ -24,11 +27,13 @@ public class GameplayState extends BasicGameState
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException
     {
+        background = new Image("res/img/background.png");
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException
     {
+        background.draw(-150, 0, background.getWidth(), 600);
     }
 
     @Override
