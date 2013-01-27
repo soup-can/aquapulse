c006f066748e47bc218e33adffd57bd9cdbde538
crynix <zachl92245>
2013-01-27 09:42:28 -0800

    Refactor :-(

--- core/src/main/java/com/soupcan/aquapulse/core/model/world/GameWorld.java
+++
@@ -1,8 +0,0 @@
-package com.soupcan.aquapulse.core.model.world;
-
-/**
- * Contains data for the game screen.
- */
-public class GameWorld extends World
-{
-}
--- core/src/main/java/com/soupcan/aquapulse/core/model/world/World.java
+++
@@ -1,8 +0,0 @@
-package com.soupcan.aquapulse.core.model.world;
-
-/**
- * Contains the data for a screen.
- */
-public abstract class World
-{
-}
